0c5f41c8c7a665b5b6aec6dba2c7dd8843f6d5d6
Adrian Kuta <adrian.kuta93@gmail.com>
2021-09-03 20:46:26 +0200

    feat: Upgrade to build gradle 7.0.2

--- app/src/main/java/com/github/adriankuta/MainActivity.kt
+++ app/src/main/java/com/github/adriankuta/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.adriankuta.databinding.ActivityMainBinding
 import com.github.adriankuta.expandable_recyclerview.expandableTree
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
         val tree = expandableTree("World") {
             child("North America") {
@@ -29,10 +31,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val adapter = ExpandableAdapter()
-        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
-        recyclerView.adapter = adapter
+        with(binding) {
+            val adapter = ExpandableAdapter()
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+            recyclerView.adapter = adapter
 
-        adapter.setTree(tree)
+            adapter.setTree(tree)
+        }
     }
 }
