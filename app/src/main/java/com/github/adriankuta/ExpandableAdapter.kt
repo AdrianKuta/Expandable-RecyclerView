3393e9b43aa769395c32574d86717b405377e8bd
Adrian Kuta <a.kuta@bsgroup.eu>
2022-11-22 18:40:07 +0100

    6: Use data model instead of String. Remove databinding.

--- app/src/main/java/com/github/adriankuta/ExpandableAdapter.kt
+++ app/src/main/java/com/github/adriankuta/ExpandableAdapter.kt
@@ -3,6 +3,7 @@ package com.github.adriankuta
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.RecyclerView
 import com.github.adriankuta.databinding.ItemLevel1Binding
 import com.github.adriankuta.databinding.ItemLevel2Binding
@@ -12,16 +13,16 @@ import com.github.adriankuta.expandable_recyclerview.ExpandableTreeNode
 import com.github.adriankuta.expandable_recyclerview.expandableTree
 
 class ExpandableAdapter :
-    ExpandableRecyclerViewAdapter<String, ExpandableAdapter.ExpandableViewHolder>() {
+    ExpandableRecyclerViewAdapter<Region, ExpandableAdapter.ExpandableViewHolder>() {
 
-    private var tree: ExpandableTreeNode<String>? = null
+    private var tree: ExpandableTreeNode<Region>? = null
 
-    fun setTree(tree: ExpandableTreeNode<String>) {
+    fun setTree(tree: ExpandableTreeNode<Region>) {
         this.tree = tree
         notifyDataSetChanged()
     }
 
-    override fun getTreeNodes(): ExpandableTreeNode<String> = tree ?: expandableTree("")
+    override fun getTreeNodes(): ExpandableTreeNode<Region> = tree ?: expandableTree(Region(""))
 
     override fun onCreateViewHolder(parent: ViewGroup, nestLevel: Int): ExpandableViewHolder {
         return when (nestLevel) {
@@ -34,7 +35,7 @@ class ExpandableAdapter :
 
     override fun onBindViewHolder(
         holder: ExpandableViewHolder,
-        treeNode: ExpandableTreeNode<String>,
+        treeNode: ExpandableTreeNode<Region>,
         nestLevel: Int
     ) {
         holder.bind(treeNode) {
@@ -46,41 +47,65 @@ class ExpandableAdapter :
 
         class Level1(private val binding: ItemLevel1Binding) : ExpandableViewHolder(binding.root) {
             override fun bind(
-                node: ExpandableTreeNode<String>,
-                onClickListener: ((ExpandableTreeNode<String>) -> Unit)?
+                node: ExpandableTreeNode<Region>,
+                onClickListener: ((ExpandableTreeNode<Region>) -> Unit)?
             ) {
-                binding.node = node
-                binding.root.setOnClickListener { onClickListener?.invoke(node) }
-                binding.executePendingBindings()
+                binding.apply {
+                    root.setOnClickListener { onClickListener?.invoke(node) }
+                    textView.text = node.value.name
+                    expandIcon.isGone = node.children.isEmpty()
+                    expandIcon.setImageResource(
+                        if (node.expanded)
+                            R.drawable.ic_expand_less_black_24dp
+                        else
+                            R.drawable.ic_expand_more_black_24dp
+                    )
+                }
             }
         }
 
         class Level2(private val binding: ItemLevel2Binding) : ExpandableViewHolder(binding.root) {
             override fun bind(
-                node: ExpandableTreeNode<String>,
-                onClickListener: ((ExpandableTreeNode<String>) -> Unit)?
+                node: ExpandableTreeNode<Region>,
+                onClickListener: ((ExpandableTreeNode<Region>) -> Unit)?
             ) {
-                binding.node = node
-                binding.root.setOnClickListener { onClickListener?.invoke(node) }
-                binding.executePendingBindings()
+                binding.apply {
+                    root.setOnClickListener { onClickListener?.invoke(node) }
+                    textView.text = node.value.name
+                    expandIcon.isGone = node.children.isEmpty()
+                    expandIcon.setImageResource(
+                        if (node.expanded)
+                            R.drawable.ic_expand_less_black_24dp
+                        else
+                            R.drawable.ic_expand_more_black_24dp
+                    )
+                }
             }
         }
 
         class Level3(private val binding: ItemLevel3Binding) : ExpandableViewHolder(binding.root) {
             override fun bind(
-                node: ExpandableTreeNode<String>,
-                onClickListener: ((ExpandableTreeNode<String>) -> Unit)?
+                node: ExpandableTreeNode<Region>,
+                onClickListener: ((ExpandableTreeNode<Region>) -> Unit)?
             ) {
-                binding.node = node
-                binding.root.setOnClickListener { onClickListener?.invoke(node) }
-                binding.executePendingBindings()
+                binding.apply {
+                    root.setOnClickListener { onClickListener?.invoke(node) }
+                    textView.text = node.value.name
+                    expandIcon.isGone = node.children.isEmpty()
+                    expandIcon.setImageResource(
+                        if (node.expanded)
+                            R.drawable.ic_expand_less_black_24dp
+                        else
+                            R.drawable.ic_expand_more_black_24dp
+                    )
+                }
             }
         }
 
 
         abstract fun bind(
-            node: ExpandableTreeNode<String>,
-            onClickListener: ((ExpandableTreeNode<String>) -> Unit)? = null
+            node: ExpandableTreeNode<Region>,
+            onClickListener: ((ExpandableTreeNode<Region>) -> Unit)? = null
         )
 
     }
